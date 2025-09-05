8bd7e527e5a7d63d92865c3d78144ca267e8e7b3
Raphael Burri <raphael.burri@isolutions.ch>
2025-09-05 15:09:25 +0200

    any

--- modules-tf/lauchpad-network/variables.tf
+++ modules-tf/lauchpad-network/variables.tf
@@ -53,7 +53,7 @@ variable "virtual_network_subnet_definitions" {
     name                  = optional(string)
     addressPrefixes       = optional(list(string))
     defaultOutboundAccess = optional(bool)
-    delegations : optional(list(object()))
+    delegations : optional(list(object(any)))
     virtualNetwork : object({
       artefactName = string
     })
