9768e44b80c918d7d97240688af1bfe41fa0c5d3
Raphael Burri <raphael.burri@isolutions.ch>
2025-09-05 11:43:26 +0200

    cidrsubnet

--- modules-tf/lauchpad-network/network.tf
+++ modules-tf/lauchpad-network/network.tf
@@ -1,8 +1,9 @@
 locals {
+  # calculate cidrsubnets on artefacts if required
   virtual_network_address_prefixes = {
     for af in toset(var.virtual_network_artefact_names) : af => {
       addressPrefixes = concat(
-        var.virtual_network_definitions[af].addressSpace.addressPrefixes,
+        try(var.virtual_network_definitions[af].addressSpace.addressPrefixes, []),
         [
           for bao in var.virtual_network_definitions[af].addressSpace.baseAddressOffsets : cidrsubnet(var.ecp_network_main_ipv4_address_space, bao.newbits, bao.netnum)
         ]
@@ -20,7 +21,6 @@ resource "azurerm_virtual_network" "lp" {
   location            = azurerm_resource_group.lp.location
   resource_group_name = azurerm_resource_group.lp.name
 
-  # address_space = var.virtual_network_definitions[each.key].addressSpace.addressPrefixesconnection {
   address_space = local.virtual_network_address_prefixes[each.key].addressPrefixes
 
   encryption {
