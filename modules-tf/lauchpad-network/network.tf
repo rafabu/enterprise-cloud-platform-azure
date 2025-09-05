b44373a39a672f44e40a73d08fd9ccf6b930cbd0
Raphael Burri <raphael.burri@isolutions.ch>
2025-09-05 11:08:43 +0200

    address_space

--- modules-tf/lauchpad-network/network.tf
+++ modules-tf/lauchpad-network/network.tf
@@ -7,9 +7,8 @@ resource "azurerm_virtual_network" "lp" {
   location            = azurerm_resource_group.lp.location
   resource_group_name = azurerm_resource_group.lp.name
 
-  address_space = [
-    var.virtual_network_definitions[each.key].addressSpace.addressPrefixes[0]
-  ]
+  address_space = var.virtual_network_definitions[each.key].addressSpace.addressPrefixes
+  
   encryption {
     enforcement = "AllowUnencrypted"
   }
