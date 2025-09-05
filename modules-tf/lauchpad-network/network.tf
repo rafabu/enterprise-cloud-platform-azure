9efba303d5f2ae4b0a17fab08213227ac3f1c1c9
Raphael Burri <raphael.burri@isolutions.ch>
2025-09-05 11:21:35 +0200

    calculate address

--- modules-tf/lauchpad-network/network.tf
+++ modules-tf/lauchpad-network/network.tf
@@ -1,3 +1,16 @@
+locals {
+  virtual_network_address_prefixes = {
+    for af in toset(var.virtual_network_artefact_names) : af => {
+      addressPrefixes = concat(
+        var.virtual_network_definitions[af].addressSpace.addressPrefixes,
+        [
+          for bao in var.virtual_network_definitions[af].addressSpace.baseAddressOffsets : cidrsubnet(var.ecp_network_main_ipv4_address_space, bao.newbits, bao.netnum)
+        ]
+      )
+    }
+  }
+}
+
 resource "azurerm_virtual_network" "lp" {
   provider = azurerm.lauchpad
 
@@ -7,8 +20,9 @@ resource "azurerm_virtual_network" "lp" {
   location            = azurerm_resource_group.lp.location
   resource_group_name = azurerm_resource_group.lp.name
 
-  address_space = var.virtual_network_definitions[each.key].addressSpace.addressPrefixes
-  
+  # address_space = var.virtual_network_definitions[each.key].addressSpace.addressPrefixesconnection {
+  address_space = local.virtual_network_address_prefixes[each.key].addressPrefixes
+
   encryption {
     enforcement = "AllowUnencrypted"
   }
@@ -16,3 +30,6 @@ resource "azurerm_virtual_network" "lp" {
 
   tags = var.azure_tags
 }
+
+
+
--- modules-tf/lauchpad-network/outputs.tf
+++ modules-tf/lauchpad-network/outputs.tf
@@ -5,12 +5,13 @@ output "resource_group" {
 
 output "virtual_networks" {
   description = "core properties of virtual networks created"
-  value       = azurerm_virtual_network.lp # {
-  #   for key, val in azurerm_virtual_network.lp : key => {
-  #     id                  = val.id,
-  #     name                = val.name,
-  #     location            = val.location
-  #     resource_group_name = val.resource_group_name
-  #   }
-  # }
+  value = {
+    for key, val in azurerm_virtual_network.lp : key => {
+      id                  = val.id,
+      name                = val.name,
+      location            = val.location
+      resource_group_name = val.resource_group_name
+      address_space       = val.address_space
+    }
+  }
 }
