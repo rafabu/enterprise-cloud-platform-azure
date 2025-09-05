e523933aa3c45a39c12405a458eeb84199797056
Raphael Burri <raphael.burri@isolutions.ch>
2025-09-05 11:05:46 +0200

    output

--- modules-tf/lauchpad-network/outputs.tf
+++ modules-tf/lauchpad-network/outputs.tf
@@ -1,9 +1,16 @@
-output "resource_group_id" {
+output "resource_group" {
   description = "The ID of the resource group"
   value       = azurerm_resource_group.lp.id
 }
 
-output "virtual_network_id" {
-  description = "The ID of the virtual network"
-  value       = azurerm_virtual_network.lp.id
+output "virtual_networks" {
+  description = "core properties of virtual networks created"
+  value       = azurerm_virtual_network.lp # {
+  #   for key, val in azurerm_virtual_network.lp : key => {
+  #     id                  = val.id,
+  #     name                = val.name,
+  #     location            = val.location
+  #     resource_group_name = val.resource_group_name
+  #   }
+  # }
 }
