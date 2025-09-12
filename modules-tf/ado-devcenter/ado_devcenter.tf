16b061fae97b2eaa4c7c5be83fe274715e7d0b2a
Raphael Burri <raphael.burri@isolutions.ch>
2025-09-12 11:31:24 +0200

    id

--- modules-tf/ado-devcenter/ado_devcenter.tf
+++ modules-tf/ado-devcenter/ado_devcenter.tf
@@ -1,77 +1,78 @@
-resource "azapi_resource" "dev_center" {
+# resource "azapi_resource" "dev_center" {
 
-  type = "Microsoft.DevCenter/devcenters@2025-02-01"
-  # managed devops pool does not (yet) exist in provider DS - just rename the RG one...
-  name      = replace(data.azurecaf_name.rg.result, "-rg-", "-devc-")
-  parent_id = data.azapi_resource_id.resource_group.id
+#   type = "Microsoft.DevCenter/devcenters@2025-02-01"
+#   # managed devops pool does not (yet) exist in provider DS - just rename the RG one...
+#   name      = replace(data.azurecaf_name.rg.result, "-rg-", "-devc-")
+#   parent_id = data.azapi_resource_id.resource_group.id
 
-  identity {
-    type = "systemAssignedIdentity"
-  }
-  location = var.azure_location
-  tags     = var.azure_location
+#   identity {
+#     type = "SystemAssigned"
+#   }
+#   location = var.azure_location
+#   tags     = var.azure_tags
 
-  body = {
-    properties = {
-      devBoxProvisioningSettings = {
-        installAzureMonitorAgentEnableStatus = "Enabled"
-      }
-      displayName = replace(data.azurecaf_name.rg.result, "-rg-", "-devc-")
-      encryption  = null
-      networkSettings = {
-        # pools will need self-hosted network
-        microsoftHostedNetworkEnableStatus = "Disabled"
-      }
-      projectCatalogSettings = {
-        catalogItemSyncEnableStatus = "Enabled"
-      }
-    }
-  }
-}
+#   body = {
+#     properties = {
+#       devBoxProvisioningSettings = {
+#         installAzureMonitorAgentEnableStatus = "Enabled"
+#       }
+#       displayName = replace(data.azurecaf_name.rg.result, "-rg-", "-devc-")
+#       encryption  = null
+#       networkSettings = {
+#         # pools will need self-hosted network
+#         microsoftHostedNetworkEnableStatus = "Disabled"
+#       }
+#       projectCatalogSettings = {
+#         catalogItemSyncEnableStatus = "Enabled"
+#       }
+#     }
+#   }
+# }
 
-resource "azapi_resource" "dev_center_project" {
+# resource "azapi_resource" "dev_center_project" {
 
-  # managed devops project does not (yet) exist in provider DS - just rename the RG one...
-  name      = replace(data.azurecaf_name.rg.result, "-rg-", "-devcproj-")
-  parent_id = data.azapi_resource_id.resource_group.id
+#   type = "Microsoft.DevCenter/projects@2025-07-01-preview"
+#   # managed devops project does not (yet) exist in provider DS - just rename the RG one...
+#   name      = replace(data.azurecaf_name.rg.result, "-rg-", "-devcproj-")
+#   parent_id = data.azapi_resource_id.resource_group.id
 
-  identity {
-    type = "systemAssignedIdentity"
-  }
-  location = var.azure_location
-  tags     = var.azure_location
+#   identity {
+#     type = "SystemAssigned"
+#   }
+#   location = var.azure_location
+#   tags     = var.azure_tags
 
-  body = {
-    properties = {
-      azureAiServicesSettings = {
-        azureAiServicesMode = "Disabled"
-      }
-      catalogSettings = {
-        catalogItemSyncTypes = [
-          "EnvironmentDefinition",
-          "ImageDefinition"
-        ]
-      }
-      customizationSettings = null
+#   body = {
+#     properties = {
+#       azureAiServicesSettings = {
+#         azureAiServicesMode = "Disabled"
+#       }
+#       catalogSettings = {
+#         catalogItemSyncTypes = [
+#           "EnvironmentDefinition",
+#           "ImageDefinition"
+#         ]
+#       }
+#       customizationSettings = null
 
-      description = "ECP Launchpad Project"
+#       description = "ECP Launchpad Project"
 
-      devBoxAutoDeleteSettings = {
-        deleteMode        = "string"
-        gracePeriod       = "string"
-        inactiveThreshold = "string"
-      }
-      devCenterId = azapi_resource.dev_center.id
-      displayName = replace(data.azurecaf_name.rg.result, "-rg-", "-devcproj-")
+#       devBoxAutoDeleteSettings = {
+#         deleteMode        = "string"
+#         gracePeriod       = "string"
+#         inactiveThreshold = "string"
+#       }
+#       devCenterId = azapi_resource.dev_center.id
+#       displayName = replace(data.azurecaf_name.rg.result, "-rg-", "-devcproj-")
 
-      maxDevBoxesPerUser = 1
-      serverlessGpuSessionsSettings = {
-        maxConcurrentSessionsPerProject = 1
-        serverlessGpuSessionsMode       = "Disabled"
-      }
-      workspaceStorageSettings = {
-        workspaceStorageMode = "Disabled"
-      }
-    }
-  }
-}
+#       maxDevBoxesPerUser = 1
+#       serverlessGpuSessionsSettings = {
+#         maxConcurrentSessionsPerProject = 1
+#         serverlessGpuSessionsMode       = "Disabled"
+#       }
+#       workspaceStorageSettings = {
+#         workspaceStorageMode = "Disabled"
+#       }
+#     }
+#   }
+# }
