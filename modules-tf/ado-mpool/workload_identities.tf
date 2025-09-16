b5398f48b2e2a58e3894060749fc4246d3bb40cf
Raphael Burri <43059250+rafabu@users.noreply.github.com>
2025-09-16 18:30:59 +0200

    each.value["scope"]

--- modules-tf/ado-mpool/workload_identities.tf
+++ modules-tf/ado-mpool/workload_identities.tf
@@ -365,8 +365,9 @@ resource "azurerm_role_assignment" "mpool" {
 
   for_each = local.ado_wid_azure_roleassigment_objects
 
-  scope              = each.value["scope"]
-  role_definition_id = format("%s/providers/Microsoft.Authorization/roleDefinitions/%s", data.azapi_client_config.this.subscription_resource_id, each.value["roleDefinitionId"])
+  scope = each.value["scope"]
+  # if scope is management group, subscription must not be added at the beginning
+  role_definition_id = format("%s/providers/Microsoft.Authorization/roleDefinitions/%s", startswith(each.value["scope"], "/subscriptions/") ? data.azapi_client_config.this.subscription_resource_id : "", each.value["roleDefinitionId"])
   principal_id       = var.workload_identity_type == "userAssignedIdentity" ? azurerm_user_assigned_identity.mpool[each.value["wid_key"]].principal_id : var.workload_identity_type == "serviceprincipal" ? azuread_service_principal.mpool[each.value["wid_key"]].object_id : "error"
 }
 
