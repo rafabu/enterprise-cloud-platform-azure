d583d84ccca8651da5d74b41b0f9b5f02c29e405
Raphael Burri <43059250+rafabu@users.noreply.github.com>
2025-09-19 15:17:27 +0200

    variable "ecp_network_main_ipv4_address_space" {   type        = string   description = "The main IPv4 address space for the ECP network" }

--- modules-tf/devcenter/variables.tf
+++ modules-tf/devcenter/variables.tf
@@ -1,3 +1,8 @@
+variable "ecp_network_main_ipv4_address_space" {
+  type        = string
+  description = "The main IPv4 address space for the ECP network"
+}
+
 variable "azure_location" {
   type = string
 }
