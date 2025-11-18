3a3d2c3546a773c8661213f4c987095d7cc4679e
Raphael Burri <43059250+rafabu@users.noreply.github.com>
2025-11-18 13:16:12 +0100

    command again

--- modules-tf/ado-repo-sync/ado-repo-sync.tf
+++ modules-tf/ado-repo-sync/ado-repo-sync.tf
@@ -20,9 +20,12 @@ resource "terraform_data" "repo_sync" {
   }
   
   provisioner "local-exec" {
-    interpreter = ["pwsh", "-NoLogo", "-NonInteractive", "-ExecutionPolicy", "RemoteSigned", "-file"]
+    # interpreter = ["pwsh", "-NoLogo", "-NonInteractive", "-ExecutionPolicy", "RemoteSigned", "-file"]
     #command = "./scripts/sync-repository.ps1 -LocalSubmodulePath '${var.local_submodule_path}' -AdoOrg '${var.ecp_azure_devops_organization_name}' -AdoProject '${var.ecp_azure_devops_project_name}' -AdoRepo '${var.ecp_azure_devops_repository_name}' -TargetBranch '${var.ecp_azure_devops_target_branch}' -ForceSync ${var.force_sync}"
-    command = "./scripts/sync-repository.ps1"
+    # command = "./scripts/sync-repository.ps1"
+    interpreter = ["pwsh", "-NoLogo", "-NonInteractive", "-ExecutionPolicy", "RemoteSigned", "-Command"]
+    command = "& './scripts/sync-repository.ps1' -LocalSubmodulePath '${var.local_submodule_path}' -AdoOrg '${var.ecp_azure_devops_organization_name}' -AdoProject '${var.ecp_azure_devops_project_name}' -AdoRepo '${var.ecp_azure_devops_repository_name}' -TargetBranch '${var.ecp_azure_devops_target_branch}' -ForceSync $${var.force_sync}"
+    
     
     environment = {
       # Azure DevOps authentication will use the existing az cli context
