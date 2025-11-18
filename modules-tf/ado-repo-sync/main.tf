fd073d26e90feb547f7df4fcf48c4a672e011858
Raphael Burri <43059250+rafabu@users.noreply.github.com>
2025-11-18 11:45:40 +0100

    required providers from terragrunt

--- modules-tf/ado-repo-sync/main.tf
+++ modules-tf/ado-repo-sync/main.tf
@@ -1,14 +1,14 @@
-terraform {
-  required_providers {
-    azuredevops = {
-      source  = "microsoft/azuredevops"
-      version = ">= 1.0.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 2.0"
-    }
-  }
-}
+# terraform {
+#   # required_providers {
+#   #   azuredevops = {
+#   #     source  = "microsoft/azuredevops"
+#   #     version = ">= 1.0.0"
+#   #   }
+#   #   local = {
+#   #     source  = "hashicorp/local"
+#   #     version = ">= 2.0"
+#   #   }
+#   # }
+# }
 
 # data "azuredevops_client_config" "this" {}
