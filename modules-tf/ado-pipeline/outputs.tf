aad45e2a3e121f7ed6112201039e1ccaadeabe0b
Raphael Burri <43059250+rafabu@users.noreply.github.com>
2025-11-19 14:51:33 +0100

    var.ado_yaml_pipeline_definitions[each.key]

--- modules-tf/ado-pipeline/outputs.tf
+++ modules-tf/ado-pipeline/outputs.tf
@@ -2,9 +2,10 @@ output "yaml_pipelines" {
   description = "core properties of Azure DevOps YAML pipelines created"
   value = {
     for key, val in azuredevops_build_definition.pipelines : key => {
-      id                  = val.id,
-      name                = val.name,
-      path                = val.path
+      id         = val.id,
+      name       = val.name,
+      path       = val.path,
+      repository = val.repository
     }
   }
 }
