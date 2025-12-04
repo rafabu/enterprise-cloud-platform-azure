a23968a4714962f1f506c185fa3f7758d7c8d483
Raphael Burri <43059250+rafabu@users.noreply.github.com>
2025-12-04 08:24:29 +0100

    az-ecp-parent

---
+++ modules-tf/az-ecp-parent/outputs.tf
@@ -0,0 +1,4 @@
+output "ecp_environment_name" {
+  description = "The name of the ECP environment"
+  value       = var.ecp_environment_name
+}
---
+++ modules-tf/az-ecp-parent/variables.tf
@@ -0,0 +1,23 @@
+variable "ecp_environment_name" {
+  type        = string
+  description = "Name of the ECP environment (used for naming resources)"
+}
+
+variable "azure_location" {
+  type = string
+}
+
+variable "azure_resource_name_elements" {
+  type = object({
+    prefixes      = optional(list(string))
+    suffixes      = optional(list(string))
+    name          = optional(string)
+    random_length = optional(number)
+  })
+  description = "Object containing naming components to be used by the azurecaf_name data source to generate resource names."
+}
+
+variable "azure_tags" {
+  type    = map(string)
+  default = {}
+}
