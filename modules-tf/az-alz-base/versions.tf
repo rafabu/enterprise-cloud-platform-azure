6e87887c31e359a1b2cbfe664ed360ac91bda930
Raphael Burri <43059250+rafabu@users.noreply.github.com>
2025-12-10 16:49:58 +0100

    versions

--- modules-tf/az-alz-base/versions.tf
+++ modules-tf/az-alz-base/versions.tf
@@ -1,1 +1,30 @@
-
+terraform {
+  required_version = ">= 1.14"
+  required_providers {
+    required_providers {
+    alz = {
+      source  = "azure/alz"
+      version = "~> 0.20"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 2.7"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.6"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
+    }
+    modtm = {
+      source  = "azure/modtm"
+      version = "~> 0.3"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.13"
+    }
+  }
+}
