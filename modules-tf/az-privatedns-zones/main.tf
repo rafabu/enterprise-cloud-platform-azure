9e4ca561cc4198e330395deb6fc6608754f6d201
Raphael Burri <43059250+rafabu@users.noreply.github.com>
2025-12-15 15:20:03 +0100

    private DNS zones

---
+++ modules-tf/az-privatedns-zones/main.tf
@@ -0,0 +1,80 @@
+locals {
+  resource_group_name = data.azurecaf_name.rg.result
+
+  # connectivity (Private DNS Zones)
+  # subscription_id_connectivity     = var.ecp_connectivity_subscription_id != "00000000-0000-0000-0000-000000000000" ? var.ecp_connectivity_subscription_id : var.ecp_management_subscription_id
+}
+
+resource "azurerm_resource_group" "private_dns_zones" {
+  provider = azurerm.connectivity
+
+  name     = "${data.azurecaf_name.rg.result}-privatelink-dnszones"
+  location = var.azure_location
+
+  tags = var.azure_tags
+}
+
+
+locals {
+  additional_private_dns_zones = {
+    # additional zones to be deployed to avm-ptn-network-private-link-private-dns-zones module's default parameter 'private_link_private_dns_zones'.
+    #  but was amended to work with the ALZ policy initiative 'Deploy-Private-DNS-Zones'.
+    #  last checked with:
+    #     - avm-ptn-network-private-link-private-dns-zones version 0.22.2
+    #     - Deploy-Private-DNS-Zones version 2.4.0
+    #      https://github.com/Azure/terraform-azurerm-avm-ptn-network-private-link-private-dns-zones
+    #      https://github.com/Azure/Azure-Landing-Zones-Library/blob/main/platform/alz/policy_set_definitions/Deploy-Private-DNS-Zones.alz_policy_set_definition.json
+    azure_chaos_studio = {
+      zone_name = "privatelink.chaos-studio.azure.com"
+    }
+    azure_deployment_environments = {
+      zone_name = "privatelink.devcenter.azure.com"
+    }
+  }
+  # missing from ALZ policy 2.4.0 but present in AVM 0.22.2:
+  # - privatelink.openai.azure.com
+  # - privatelink.mysql.database.azure.com
+  # - privatelink.postgres.database.azure.com
+  # - privatelink.purview.azure.com
+  # - privatelink.purviewstudio.azure.com
+  # - privatelink.grafana.azure.com
+  # - privatelink.azurestaticapps.net
+  # - privatelink.fhir.azurehealthcareapis.com
+  # - privatelink.dicom.azurehealthcareapis.com
+}
+
+
+module "private_dns_zones" {
+  providers = {
+  }
+
+  source  = "Azure/avm-ptn-network-private-link-private-dns-zones/azurerm"
+  version = "0.22.2"
+
+  location  = var.azure_location
+  parent_id = module.alz_network.network_management_group_id
+
+  private_link_excluded_zones = []
+  # private_link_private_dns_zones <<== leave default values
+  # private_link_private_dns_zones = {}
+  private_link_private_dns_zones_additional = {
+    for key, val in local.additional_private_dns_zones : key => {
+      zone_name                              = val
+      private_dns_zone_supports_private_link = true
+    }
+  }
+
+  resource_group_role_assignments = null
+
+  virtual_network_link_default_virtual_networks = [
+    for vnetid in var.virtual_network_link_id_list : {
+      virtual_network_resource_id = vnetid
+    }
+  ]
+
+  virtual_network_link_resolution_policy_default = "NxDomainRedirect"
+
+  enable_telemetry = false
+
+  tags = module.private_dns_zones_rg[0].resource.tags
+}
---
+++ modules-tf/az-privatedns-zones/variables.tf
@@ -0,0 +1,59 @@
+variable "azure_location" {
+  type        = string
+  description = "Default region for resources deployed into this subscription."
+}
+
+variable "azure_resource_name_elements" {
+  type = object({
+    prefixes      = optional(list(string))
+    suffixes      = optional(list(string))
+    name          = optional(string)
+    random_length = optional(number)
+  })
+  description = "Object containing naming components to be used by the azurecaf_name data source to generate resource names."
+}
+
+variable "azure_tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "ecp_environment_name" {
+  type        = string
+  description = "Name of the ECP environment (used for naming resources)"
+}
+
+variable "ecp_launchpad_subscription_id" {
+  type        = string
+  description = "The identifier of the Azure Subscription. (e.g '00000000-0000-0000-0000-000000000000')"
+  validation {
+    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", var.ecp_launchpad_subscription_id))
+    error_message = "The subscription ID must be a valid GUID in the format '00000000-0000-0000-0000-000000000000'."
+  }
+}
+
+variable "ecp_management_subscription_id" {
+  type        = string
+  description = "The identifier of the Azure Subscription. (e.g '00000000-0000-0000-0000-000000000000')"
+  validation {
+    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", var.ecp_management_subscription_id))
+    error_message = "The subscription ID must be a valid GUID in the format '00000000-0000-0000-0000-000000000000'."
+  }
+}
+
+variable "ecp_connectivity_subscription_id" {
+  type        = string
+  description = "The identifier of the Azure Subscription. (e.g '00000000-0000-0000-0000-000000000000')"
+  validation {
+    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", var.ecp_connectivity_subscription_id))
+    error_message = "The subscription ID must be a valid GUID in the format '00000000-0000-0000-0000-000000000000'."
+  }
+}
+
+variable "virtual_network_link_id_list" {
+  type        = list(string)
+  default     = [
+    "/subscriptions/e1b3be0d-0df0-4e0a-a585-ffc97f60bd42/resourceGroups/rabu-d7-rg-ecpalp-main/providers/Microsoft.Network/virtualNetworks/rabu-d7-vnet-ecpalp-main"
+  ]
+  description = "vNet IDs to be linked to Private DNS Zones deployed with this module."
+}
