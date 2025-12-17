339080e5fe8160f1847809733ff16467a3772710
Raphael Burri <43059250+rafabu@users.noreply.github.com>
2025-12-17 11:31:10 +0100

    Refactor output value assignment for ALZ library artefact templating

--- modules-tf/az-alz-shared-library-render/outputs.tf
+++ modules-tf/az-alz-shared-library-render/outputs.tf
@@ -1,3 +1,3 @@
 output "alz_library_artefact_templating_result" {
-  value = toset(local.alz_library_folder_exists ? [var.alz_library_path_shared] : []) ? data.external.alz_library_artefact_templating.result : {}
+  value = local.alz_library_folder_exists ? data.external.alz_library_artefact_templating[var.alz_library_path_shared].result : {}
 }
