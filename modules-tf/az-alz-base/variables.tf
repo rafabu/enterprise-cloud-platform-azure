2033c9bddc4560f870f5aeb084756a34e2410f68
Raphael Burri <43059250+rafabu@users.noreply.github.com>
2025-12-19 20:35:19 +0100

    Remove commented-out variables and clean up default values in Terraform configuration

--- modules-tf/az-alz-base/variables.tf
+++ modules-tf/az-alz-base/variables.tf
@@ -3,21 +3,6 @@ variable "azure_location" {
   description = "Default region for resources deployed into this subscription."
 }
 
-# variable "azure_resource_name_elements" {
-#   type = object({
-#     prefixes      = optional(list(string))
-#     suffixes      = optional(list(string))
-#     name          = optional(string)
-#     random_length = optional(number)
-#   })
-#   description = "Object containing naming components to be used by the azurecaf_name data source to generate resource names."
-# }
-
-# variable "azure_tags" {
-#   type    = map(string)
-#   default = {}
-# }
-
 variable "ecp_environment_name" {
   type        = string
   description = "Name of the ECP environment (used for naming resources)"
@@ -78,12 +63,6 @@ variable "ecp_security_subscription_id" {
   }
 }
 
-# variable "ecp_alz_avm_version" {
-#     type = string
-#     default = "0.15.0"
-#     description = "Version of the AVM PTN ALZ module 'avm-ptn-alz' to use. See https://github.com/Azure/terraform-azurerm-avm-ptn-alz/releases for available versions."
-# }
-
 variable "ecp_alz_architecture_name" {
   type        = string
   default     = "ecp-alz-v1"
@@ -105,14 +84,7 @@ variable "alz_management_resource_ids" {
     ama_user_assigned_managed_identity_id       = optional(string)
     ddos_protection_plan_id                     = optional(string)
   })
-  default = {
-    # ama_change_tracking_data_collection_rule_id = "/subscriptions/5c838b6a-9149-423a-9de4-ff1682f70388/resourceGroups/rabu-d7-rg-ecpa-mgmt/providers/Microsoft.Insights/dataCollectionRules/rabu-d7-dcr-ecpa-mgmt-change-tracking"
-    # ama_defender_sqls_data_collection_rule_id   = "/subscriptions/5c838b6a-9149-423a-9de4-ff1682f70388/resourceGroups/rabu-d7-rg-ecpa-mgmt/providers/Microsoft.Insights/dataCollectionRules/rabu-d7-dcr-ecpa-mgmt-defender-sql"
-    # ama_user_assigned_identity_id               = "/subscriptions/5c838b6a-9149-423a-9de4-ff1682f70388/resourceGroups/rabu-d7-rg-ecpa-mgmt/providers/Microsoft.ManagedIdentity/userAssignedIdentities/rabu-d7-id-ecpa-mgmt-ama"
-    # ama_vm_insights_data_collection_rule_id     = "/subscriptions/5c838b6a-9149-423a-9de4-ff1682f70388/resourceGroups/rabu-d7-rg-ecpa-mgmt/providers/Microsoft.Insights/dataCollectionRules/rabu-d7-dcr-ecpa-mgmt-vm-insights"
-    # log_analytics_workspace_id                  = "/subscriptions/5c838b6a-9149-423a-9de4-ff1682f70388/resourceGroups/rabu-d7-rg-ecpa-mgmt/providers/Microsoft.OperationalInsights/workspaces/rabu-d7-log-ecpa-mgmt"
-    # ddos_protection_plan_id                     = null
-  }
+  default     = {}
   description = "Objects mapping management resource key to resource Id,"
 }
 
