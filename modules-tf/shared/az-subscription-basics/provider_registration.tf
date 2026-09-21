6a823363c4a014787f1b7f3231ff430ca1a12453
Raphael Burri <43059250+rafabu@users.noreply.github.com>
2026-09-22 00:10:02 +0200

    register required providers on platform subscriptions

---
+++ modules-tf/shared/az-subscription-basics/provider_registration.tf
@@ -0,0 +1,87 @@
+locals {
+  # non-standard subscription providers that need to be registered during "apply"
+  #     use azapi to allow opportunistic registration
+  subscription_resource_providers = [
+    "Microsoft.Compute",
+    "Microsoft.GuestConfiguration",
+    "Microsoft.KeyVault",
+    "Microsoft.ManagedIdentity",
+    "Microsoft.Network",
+    "Microsoft.OperationalInsights",
+    "Microsoft.OperationsManagement",
+    "Microsoft.PolicyInsights",
+    "Microsoft.Quota",
+    "Microsoft.Security",
+    "Microsoft.Storage"
+  ]
+}
+
+# register provider may be a slow operation
+#     Assist terraform by actively assure it is registered and
+#     add a sleep. Otherwise resource operations will fail initially.
+#     Use only data-sources to not block enrolment should
+#     registration already have happened.
+data "azapi_resource" "provider_registration" {
+  for_each = toset(local.subscription_resource_providers)
+
+  type        = format("%s@2025-04-01", each.key)
+  resource_id = "/subscriptions/${var.subscription_id}/providers/${each.key}"
+
+  response_export_values = [
+    "namespace",
+    "registrationPolicy",
+    "registrationState"
+  ]
+}
+
+data "azapi_resource_action" "provider_registration" {
+  for_each = toset(local.subscription_resource_providers)
+
+  type        = format("%s@2025-04-01", each.key)
+  resource_id = "/subscriptions/${var.subscription_id}/providers/${each.key}"
+
+  # only register if it is currently unregistered (empty action does nothing but a get)
+  action = contains(["NotRegistered", "Unregistered"], data.azapi_resource.provider_registration[each.key].output.registrationState) ? "Register" : ""
+  method = contains(["NotRegistered", "Unregistered"], data.azapi_resource.provider_registration[each.key].output.registrationState) ? "POST" : "GET"
+
+  response_export_values = [
+    "namespace",
+    "registrationPolicy",
+    "registrationState"
+  ]
+}
+
+resource "time_sleep" "wait_after_provider_register" {
+  for_each = toset(local.subscription_resource_providers)
+
+  # 5 mins sleep timer to allow slow provider registration to finish
+  create_duration = data.azapi_resource.provider_registration[each.key].output.registrationState != "Registered" ? "5m" : "1ms"
+
+  triggers = {
+    create_duration = data.azapi_resource.provider_registration[each.key].output.registrationState != "Registered" ? "5m" : "1ms"
+  }
+}
+
+data "azapi_resource" "provider_registration_recheck" {
+  for_each = toset(local.subscription_resource_providers)
+
+  type        = format("%s@2025-04-01", each.key)
+  resource_id = "/subscriptions/${var.subscription_id}/providers/${each.key}"
+
+  response_export_values = [
+    "namespace",
+    "registrationPolicy",
+    "registrationState"
+  ]
+
+  lifecycle {
+    postcondition {
+      condition     = self.output.registrationState == "Registered"
+      error_message = format("ERROR: Subscription provider '%s' is currently in state '%s'. Cannot provision any resources until it has been successfully registered.", self.output.namespace, self.output.registrationState)
+    }
+  }
+
+  depends_on = [
+    time_sleep.wait_after_provider_register
+  ]
+}
