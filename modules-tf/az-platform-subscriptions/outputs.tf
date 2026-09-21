60bbfa0b952ba94ed7609dda3374d12059959b72
Raphael Burri <43059250+rafabu@users.noreply.github.com>
2026-09-21 19:37:03 +0200

    subscription module (#57)

--- modules-tf/az-platform-subscriptions/outputs.tf
+++ modules-tf/az-platform-subscriptions/outputs.tf
@@ -1,24 +1,24 @@
 output "launchpad_subscription" {
   description = "Subscription details including tags and metadata"
-  value       = var.ecp_launchpad_subscription_id != "00000000-0000-0000-0000-000000000000" ? module.launchpad_subscription["this"].subscription : null
+  value       = try(module.launchpad_subscription["this"].subscription, null)
 }
 
 output "management_subscription" {
   description = "Subscription details including tags and metadata"
-  value       = var.ecp_management_subscription_id != "00000000-0000-0000-0000-000000000000" ? module.management_subscription["this"].subscription : null
+  value       = module.management_subscription["this"].subscription
 }
 
 output "connectivity_subscription" {
   description = "Subscription details including tags and metadata"
-  value       = var.ecp_connectivity_subscription_id != "00000000-0000-0000-0000-000000000000" ? module.connectivity_subscription["this"].subscription : null
+  value       = try(module.connectivity_subscription["this"].subscription, null)
 }
 
 output "identity_subscription" {
   description = "Subscription details including tags and metadata"
-  value       = var.ecp_identity_subscription_id != "00000000-0000-0000-0000-000000000000" ? module.identity_subscription["this"].subscription : null
+  value       = try(module.identity_subscription["this"].subscription, null)
 }
 
 output "security_subscription" {
   description = "Subscription details including tags and metadata"
-  value       = var.ecp_security_subscription_id != "00000000-0000-0000-0000-000000000000" ? module.security_subscription["this"].subscription : null
+  value       = try(module.security_subscription["this"].subscription, null)
 }
