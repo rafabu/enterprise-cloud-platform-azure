468dfdb08cf9bf2da089086a671d301f707fa871
Raphael Burri <43059250+rafabu@users.noreply.github.com>
2026-09-25 01:30:15 +0200

    Feature/intermediate root on destroy (#64)
    * intermediate root
    
    * custom MG root on delete

--- modules-tf/az-alz-base/main.tf
+++ modules-tf/az-alz-base/main.tf
@@ -41,8 +41,8 @@ module "alz" {
     } : {}
   )
 
-  subscription_placement_destroy_behavior = "intermediate_root"
-  # subscription_placement_destroy_custom_target_management_group_id = var.ecp_azure_root_parent_management_group_id
+  subscription_placement_destroy_behavior                          = "custom"
+  subscription_placement_destroy_custom_target_management_group_id = var.ecp_azure_root_parent_management_group_id
 
   #   management_group_hierarchy_settings = {
   #     default_management_group_name            = "sandbox"
